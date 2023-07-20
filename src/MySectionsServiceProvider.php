284249e65ca1badf9f7a76d21a87cf46b971996e
Emilien Hodee <hodeeemilien@gmail.com>
2023-07-20 19:05:13 +0200

    ServiceProvider publishables

--- src/MySectionsServiceProvider.php
+++ src/MySectionsServiceProvider.php
@@ -5,49 +5,28 @@ namespace JoliMardi\MySections;
 use Illuminate\Support\ServiceProvider;
 
 class MySectionsServiceProvider extends ServiceProvider {
-    /**
-     * Bootstrap the application services.
-     *
-     * @return void
-     */
     public function boot() {
-        // Publish Nova Resources
+
+        $this->loadViewsFrom(__DIR__ . '/views', 'mysections');
+
+        // Nova + models
         $this->publishes([
             __DIR__ . '/Nova' => app_path('Nova'),
-        ], 'nova-resources');
+            __DIR__ . '/Models' => app_path('Models'),
+        ], 'nova');
 
-        // Publish views
+        // Publish views + css
         $this->publishes([
             __DIR__ . '/views' => resource_path('views/sections'),
+            __DIR__ . '/css' => resource_path('css/custom'),
         ], 'views');
 
         // Publish migrations
         $this->publishes([
             __DIR__ . '/migrations' => database_path('migrations'),
-        ], 'migrations');
-
-        // Publish CSS
-        $this->publishes([
-            __DIR__ . '/css' => public_path('css/mysections'),
-        ], 'public');
-
-        // Publish Models
-        $this->publishes([
-            __DIR__ . '/Models' => app_path('Models'),
-        ], 'models');
-
-        // Load migrations if not published
-        $this->loadMigrationsFrom(__DIR__ . '/migrations');
-
-        // Load views if not published
-        $this->loadViewsFrom(__DIR__ . '/views', 'mysections');
+        ], 'mysections-migrations');
     }
 
-    /**
-     * Register the application services.
-     *
-     * @return void
-     */
     public function register() {
     }
 }
---
+++ src/Nova/SectionMaxWidth.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Nova;
+
+use Illuminate\Http\Request;
+use Laravel\Nova\Fields\ID;
+use Laravel\Nova\Fields\Text;
+use Laravel\Nova\Http\Requests\NovaRequest;
+
+class SectionMaxWidth extends Resource {
+    /**
+     * The model the resource corresponds to.
+     *
+     * @var class-string<\App\Models\SectionMaxWidth>
+     */
+    public static $model = \App\Models\SectionMaxWidth::class;
+
+    /**
+     * The single value that should be used to represent the resource when being displayed.
+     *
+     * @var string
+     */
+    public static $title = 'name';
+
+    /**
+     * The columns that should be searched.
+     *
+     * @var array
+     */
+    public static $search = [
+        'name', 'class'
+    ];
+
+
+    public static $displayInNavigation = false;
+
+    /**
+     * Get the fields displayed by the resource.
+     *
+     * @param  \Laravel\Nova\Http\Requests\NovaRequest  $request
+     * @return array
+     */
+    public function fields(NovaRequest $request) {
+        return [
+            ID::make()->sortable()->hideFromIndex(),
+            Text::make('Nom de la classe', 'name')->sortable()->copyable(),
+            Text::make('Classe', 'class')->sortable(),
+        ];
+    }
+
+    /**
+     * Get the cards available for the request.
+     *
+     * @param  \Laravel\Nova\Http\Requests\NovaRequest  $request
+     * @return array
+     */
+    public function cards(NovaRequest $request) {
+        return [];
+    }
+
+    /**
+     * Get the filters available for the resource.
+     *
+     * @param  \Laravel\Nova\Http\Requests\NovaRequest  $request
+     * @return array
+     */
+    public function filters(NovaRequest $request) {
+        return [];
+    }
+
+    /**
+     * Get the lenses available for the resource.
+     *
+     * @param  \Laravel\Nova\Http\Requests\NovaRequest  $request
+     * @return array
+     */
+    public function lenses(NovaRequest $request) {
+        return [];
+    }
+
+    /**
+     * Get the actions available for the resource.
+     *
+     * @param  \Laravel\Nova\Http\Requests\NovaRequest  $request
+     * @return array
+     */
+    public function actions(NovaRequest $request) {
+        return [];
+    }
+}
