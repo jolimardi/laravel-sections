861b7614c222baf1bf544fdafe555b1e06a89285
Romain Chérot <romain.cherot@free.fr>
2023-09-28 11:50:17 +0200

    Youtube nocookie

--- src/views/components/youtube-inline.blade.php
+++ src/views/components/youtube-inline.blade.php
@@ -1,7 +1,7 @@
 @if (isset($youtube))
     <div class="video-thumbnail ratio-16x9">
 
-        <iframe src="https://www.youtube.com/embed/{{ $youtube }}?&loop=1&playlist={{ $youtube }}&rel=0&controls=1&autoplay=1&mute=1&start=0" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen=""></iframe>
+        <iframe src="https://www.youtube-nocookie.com/embed/{{ $youtube }}?&loop=1&playlist={{ $youtube }}&rel=0&controls=1&autoplay=1&mute=1&start=0" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen=""></iframe>
 
     </div>
 @endif
