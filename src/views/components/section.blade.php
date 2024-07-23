8fa65ac77cbd56706a8365b509ba8672f33f7dae
Romain Chérot <romain.cherot@free.fr>
2024-07-23 15:57:39 +0200

    Nouveaux raccourcis pour les sections (small, large, gray, grayAlt, green, gradient)

--- src/views/components/section.blade.php
+++ src/views/components/section.blade.php
@@ -1,12 +1,46 @@
 @props([
-    'maxWidth' => 'medium',
+    'maxWidth' => false,
     'bg' => '',
     'maxWidthClasses' => [
         'small' => 'max-width-small',
         'medium' => 'max-width',
         'large' => 'max-width-large',
     ],
+    'gray' => false,
+    'grayAlt' => false,
+    'green' => false,
+    'gradient' => false,
+
+    'small' => false,
+    'large' => false,
 ])
+
+<?php
+if (!$maxWidth) {
+    $maxWidth = 'medium';
+    if ($small) {
+        $maxWidth = 'small';
+    }
+    if ($large) {
+        $maxWidth = 'large';
+    }
+}
+
+if (empty($bg)) {
+    if ($gray) {
+        $bg = 'gray';
+    }
+    if ($grayAlt) {
+        $bg = 'gray-alt';
+    }
+    if ($green) {
+        $bg = 'green';
+    }
+    if ($gradient) {
+        $bg = 'gradient';
+    }
+}
+?>
 
 <div {{ $attributes->merge(['class' => "section {$bg}"]) }}>
     <div class="{{ $maxWidthClasses[$maxWidth] }}">
